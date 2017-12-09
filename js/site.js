e164bbace2a1c0f409175f93f36d38ce96eaebbc
Michiel Leunens <michiel.leunens.y7743@student.hogent.be>
2017-12-09 19:15:14 +0100

    Update

--- js/site.js
+++ js/site.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
         $(this).toggleClass('open');
     });
 });
+
+$('.carousel').carousel();
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 500) {
+        $('.navbar').css('background', '#343a40');
+    } else {
+        $('.navbar').css('background', 'transparent');
+    }
+});
