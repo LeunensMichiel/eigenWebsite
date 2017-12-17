1dacce052cbb33bc8ddfcce9f92bfe3c81254b14
Michiel Leunens <michiel.leunens.y7743@student.hogent.be>
2017-12-17 19:06:07 +0100

    kleine fix ivm de navtoggler

--- js/site.js
+++ js/site.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
-        $(this).toggleClass('open');
+    var state = false;
+    $('#nav-icon3').click(function () {
+        if (state === false) {
+            $(this).toggleClass('open');
+            $('.navbar').css('background', '#343a40');
+            state = true;
+        } else {
+            $(this).toggleClass('open');
+            $('.navbar').css('background', 'transparent');
+            state = false;
+        }
     });
 });
 
@@ -13,3 +22,18 @@ $(window).scroll(function () {
         $('.navbar').css('background', 'transparent');
     }
 });
+
+function initMap() {
+    var uluru = {
+        lat: 51.052260,
+        lng: 3.717568
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
